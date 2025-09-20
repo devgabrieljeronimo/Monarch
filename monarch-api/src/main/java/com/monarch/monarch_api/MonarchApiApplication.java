dcadb6129049bbf703e249cfa4b273ee91961d26
Gabriel Jeronimo <gabrieljeronimo2b.007@gmail.com>
2025-09-20 17:24:53 -0300

    Initial commit

---
+++ monarch-api/src/main/java/com/monarch/monarch_api/MonarchApiApplication.java
@@ -0,0 +1,13 @@
+package com.monarch.monarch_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonarchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonarchApiApplication.class, args);
+	}
+
+}
