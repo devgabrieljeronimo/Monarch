8beeda9bd6918dcca5dec60d62712e2b6d55e135
Gabriel Jeronimo <gabrieljeronimo2b.007@gmail.com>
2025-09-25 16:10:16 -0300

    Upgrade UserController

--- monarch-api/src/main/java/com/monarch/monarch_api/controller/UserController.java
+++ monarch-api/src/main/java/com/monarch/monarch_api/controller/UserController.java
@@ -1,4 +1,10 @@
 package com.monarch.monarch_api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
 public class UserController {
+
 }
