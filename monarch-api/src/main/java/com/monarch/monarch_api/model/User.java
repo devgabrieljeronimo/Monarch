ffc93b2030c1ac2a27eec0c8a1c54ab08bba50dd
Gabriel Jeronimo <gabrieljeronimo2b.007@gmail.com>
2025-09-25 15:58:05 -0300

    Update model User

--- monarch-api/src/main/java/com/monarch/monarch_api/model/User.java
+++ monarch-api/src/main/java/com/monarch/monarch_api/model/User.java
@@ -5,5 +5,23 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "user")
 public class User {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private Integer birthdate;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
 }
