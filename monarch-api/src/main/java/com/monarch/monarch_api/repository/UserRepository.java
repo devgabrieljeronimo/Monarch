e969fa28ebdc6208fe733e2faeb60b306a3c0928
Gabriel Jeronimo <gabrieljeronimo2b.007@gmail.com>
2025-09-26 17:29:12 -0300

    Add findByEmail in UserRepository

--- monarch-api/src/main/java/com/monarch/monarch_api/repository/UserRepository.java
+++ monarch-api/src/main/java/com/monarch/monarch_api/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.monarch.monarch_api.repository;
 import com.monarch.monarch_api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    Optional<User> findByEmail(String email);
 }
