dbe81fe472dac6f2c32777adbf8ccdf4196632cd
Gabriel Jeronimo <gabrieljeronimo2b.007@gmail.com>
2025-09-26 16:21:14 -0300

    Add repository in UserController

--- monarch-api/src/main/java/com/monarch/monarch_api/controller/UserController.java
+++ monarch-api/src/main/java/com/monarch/monarch_api/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.monarch.monarch_api.controller;
 
+import com.monarch.monarch_api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
+
+    @Autowired
+    UserRepository repository;
 
 }
