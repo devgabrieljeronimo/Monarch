214382fd4324c2083fb078ea1f2fae762cb5c052
Gabriel Jeronimo <gabrieljeronimo2b.007@gmail.com>
2025-09-26 16:39:57 -0300

    Add @GetMapping getById in UserController

--- monarch-api/src/main/java/com/monarch/monarch_api/controller/UserController.java
+++ monarch-api/src/main/java/com/monarch/monarch_api/controller/UserController.java
@@ -1,15 +1,33 @@
 package com.monarch.monarch_api.controller;
 
+import com.monarch.monarch_api.model.User;
 import com.monarch.monarch_api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
-@RequestMapping("user")
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     UserRepository repository;
+
+    @GetMapping("/{id}")
+    public ResponseEntity getById(@PathVariable(value = "id") Long id) {
+        Optional<User> user = repository.findById(id);
+
+        if(user.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(user.get());
+    }
 
 }
