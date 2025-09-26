97e01da4c1885ce4d3637e1b922a7b2e9429f4b4
Gabriel Jeronimo <gabrieljeronimo2b.007@gmail.com>
2025-09-26 16:17:12 -0300

    Add validation in UserDto

--- monarch-api/src/main/java/com/monarch/monarch_api/dto/UserDto.java
+++ monarch-api/src/main/java/com/monarch/monarch_api/dto/UserDto.java
@@ -1,14 +1,28 @@
 package com.monarch.monarch_api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
 public record UserDto(
 
+        @NotBlank(message = "This field is required!")
+        @Size(max = 16, message = "Name too long!")
         String name,
 
+        @NotBlank(message = "This field is required!")
+        @Size(max = 25, message = "Last name too long!")
         String lastName,
 
-        Integer birthdate,
+        @NotBlank(message = "This field is required!")
+        LocalDate birthdate,
 
+        @NotBlank(message = "This field is required!")
+        @Size(max = 100, message = "Email too long!")
         String email,
 
+        @NotBlank(message = "This field is required!")
+        @Size(min = 8, max = 16, message = "")
         String password
 ) {}
