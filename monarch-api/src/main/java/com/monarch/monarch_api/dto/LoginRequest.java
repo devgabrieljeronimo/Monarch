382104acb094396f023969abae421dea5d5428aa
Gabriel Jeronimo <gabrieljeronimo2b.007@gmail.com>
2025-10-04 16:48:04 -0300

    Upgrade LoginRequest

--- monarch-api/src/main/java/com/monarch/monarch_api/dto/LoginRequest.java
+++ monarch-api/src/main/java/com/monarch/monarch_api/dto/LoginRequest.java
@@ -4,7 +4,15 @@ package com.monarch.monarch_api.dto;
     atualizar utilizando Spring Security assim que possivel */
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
-        
+
+        @NotBlank(message = "This field is required!")
+        @Size(max = 100, message = "Email too long!")
+        String email,
+
+        @NotBlank(message = "This field is required!")
+        @Size(min = 8, max = 16, message = "The password must be between 8 and 16 characters long!")
+        String password
 ) {}
