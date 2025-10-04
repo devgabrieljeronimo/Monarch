d6cb0edfc712c81ab87e42baf324c18dca96c56d
Gabriel Jeronimo <gabrieljeronimo2b.007@gmail.com>
2025-10-04 16:32:16 -0300

    Create LoginRequest dto

---
+++ monarch-api/src/main/java/com/monarch/monarch_api/dto/LoginRequest.java
@@ -0,0 +1,10 @@
+package com.monarch.monarch_api.dto;
+
+/* Dto temporario para iniciar testes da api,
+    atualizar utilizando Spring Security assim que possivel */
+
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+        
+) {}
